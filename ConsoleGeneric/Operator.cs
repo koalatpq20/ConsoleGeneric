330f2d60f1a09de467177453ebda564a5ece810f
Quynh Phuong Tran <103179076+koalatpq20@users.noreply.github.com>
2022-10-14 12:01:35 +0700

    Add project files.

---
+++ ConsoleGeneric/Operator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGeneric
+{
+    public  class Operator<T>
+    {
+        public void Display(T value)
+        { 
+            Console.WriteLine($"Giá trị nhập {value} - Kiểu dữ liệu: {typeof(T)}");
+        }
+    }
+}
---
+++ ConsoleGeneric/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleGeneric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Operator<int> operatorInt = new Operator<int>();
+            operatorInt.Display(100);
+
+            Operator<double> operatorDouble = new Operator<double>();
+            operatorDouble.Display(50.25);
+
+            Operator<string> operatorString = new Operator<string>();
+            operatorString.Display("Hello...");
+
+            Console.ReadLine();
+        }
+    }
+}
